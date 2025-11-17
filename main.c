5ee1e604a513ce6511a03ce65b4d2179baf8ca82
Bud <paul.linschoten@gmail.com>
2025-11-17 23:52:37 +0100

    tested empty string

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include "libasm.h"
+//#include <stdbool.h>
+
+void	check_strlen(const char *s){
+	printf("str: %s\n", s);
+	printf("ft_strlen = %ld | strlen = %ld\n\n", ft_strlen(s), strlen(s));
+	
+}
 
 int main(void){
-	const char *str1 = "This is a string\n";
-	printf("str: %s\n", str1);
-	printf("ft_strlen = %ld | strlen = %ld\n\n", ft_strlen(str1), strlen(str1));
+	const char *str1 = "This is a string";
+	const char *empty = "";
+	check_strlen(str1);
+	check_strlen(empty);
 	return 0;
 }
